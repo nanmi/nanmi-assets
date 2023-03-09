2f4f7cde89c22d520390a22f520143e251b39f1d
nanmi <yueshangChang@gmail.com>
2023-03-09 14:54:01 +0800

    update data sample code

--- python/摄像头数据采集脚本/record_camera.py
+++ python/摄像头数据采集脚本/record_camera.py
@@ -1,82 +1,92 @@
 import cv2
 import time
 import os
-import configparser
+import json
+import logging
+
+
+def set_log():
+    logger = logging.getLogger()
+    fh = logging.FileHandler("record.log", encoding="utf-8", mode="a")
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.setLevel(logging.DEBUG)
+    return logger
 
 class RecordImage(object):
     def __init__(self) -> None:
-        self.n_camera = 0
-        self.rtsp_list = {}
         self.interval = 0
+        self.bridge_list = []
+        self.logger = set_log()
     
     def time_stamp(self):
         time_stamp = time.strftime(r"%Y_%m_%d_%H_%M_%S", time.gmtime())
         return time_stamp
-
-    def camera_num(self):
-        count = 0
-        with open('./cameras.cfg', 'r', encoding='utf-8') as f:
-            line = 'a'
-            while line != '':
-                line = f.readline()
-                if line != '' and line[:3] == 'URI':
-                    count += 1
-        self.n_camera = count
-
-    def read_cfg(self):
-        self.camera_num()
-        cfg_path = './cameras.cfg'
-        cp = configparser.RawConfigParser()
-        cp.read(cfg_path, encoding='utf-8')
-        self.interval = int(cp.get('sample', 'time'))
-        for i in range(self.n_camera):
-            loca = cp.get(f'rtsp{i}', 'LOCATION')
-            uri = cp.get(f'rtsp{i}', 'URI')
-            self.rtsp_list[f'rtsp{i}'] = [str(loca), str(uri)]
-
-    def caputure_(self, save_path:str, i:int, rtsp : str) -> None:
-        try:
-            # cap = cv2.VideoCapture(self.rtsp_list[rtsp][1])
-            time_out = 250 # micro second
-            os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = f"timeout;{time_out}"
-            cap=cv2.VideoCapture(self.rtsp_list[rtsp][1], cv2.CAP_FFMPEG)
-        except Exception as e:
-            # print(e)
-            return
+    
+    def read_json(self):
+        # self.camera_num()
+        json_path = './cameras.json'
+        json_dict = None
+        with open(json_path, 'r', encoding='utf-8') as fp:
+            json_dict = json.load(fp)
+        self.interval = int(json_dict['time_inteval'])
         
+        self.bridge_list = list(json_dict.keys())
+        return json_dict
+
+    def caputure_(self, save_path:str, i:int, camera_name:str,  camera_url : str) -> None:
+        try:
+            cap = cv2.VideoCapture(camera_url)
+            # time_out = 250 # micro second
+            # os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = f"timeout;{time_out}"
+            # cap=cv2.VideoCapture(camera_url, cv2.CAP_FFMPEG)
+        except Exception as e:
+            print(e)
+            return
+        # cap = cv2.VideoCapture(camera_url)
         if not cap.isOpened():
-            print(f'{rtsp} is not opened or not online')
+            print(f'{camera_url} is not opened or not online')
             return
         
         ret, frame = cap.read()
+        
         if ret:
             time_s = self.time_stamp()
-            loca = self.rtsp_list[rtsp][0]
-            print(f'>>> 相机采集-{loca}-{time_s} - 第 {i} 张图像')
-            save_img_path = os.path.join(save_path, f'{loca}_{time_s}_{str(i).zfill(8)}.jpg')
+            print(f'>>> 相机采集-第 {i} 轮采集 - {camera_name} -{time_s} - 第 {i} 张图像')
+            self.logger.info(f'>>> 相机采集-第 {i} 轮采集 - {camera_name} -{time_s} - 第 {i} 张图像')
+            save_img_path = os.path.join(save_path, f'{camera_name}_{time_s}_{str(i).zfill(8)}.jpg')
             # cv2.imwrite(save_img_path, frame)
             cv2.imencode('.jpg', frame)[1].tofile(save_img_path)
         else:
             print('read frame failed')
             cap.release()
-        return
+        
+        cap.release()
 
     def makedir_(self, path):
+        print(path)
         if not os.path.exists(path):
             os.makedirs(path)
 
     def runtime(self):
-        self.read_cfg()
+        json_dict = self.read_json()
         i = 0
         base = './record_imgs'
-        if not os.path.exists(base):
-            os.makedirs(base)
+        self.makedir_(base)
         while True:
-            for c_i in range(self.n_camera):
-                rtsp = f'rtsp{c_i}'
-                path = os.path.join(base, self.rtsp_list[rtsp][0])
-                self.makedir_(path)
-                self.caputure_(path, i, rtsp)
+            for bridge_name in self.bridge_list[1:]:
+                bridge_path = os.path.join(base, bridge_name)
+                self.makedir_(bridge_path)
+
+                bridge_cameras_lsit = json_dict[bridge_name]
+                for camera_json in bridge_cameras_lsit:
+                    camera_name = camera_json['camera_name']
+                    camera_url = camera_json['url']
+                    camera_path = os.path.join(bridge_path, camera_name)
+                    self.makedir_(camera_path)
+                    if camera_url != "":
+                        self.caputure_(camera_path, i, camera_name, camera_url)
             i += 1
             time.sleep(self.interval)
 
